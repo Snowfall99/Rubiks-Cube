aaf5b92724a7e45f39dd41bd55f82a068ff7d749
Snowfall99 <445117445@qq.com>
2021-04-02 10:15:05 +0800

    multi light and pearl material

--- rubiks_cube/main.cpp
+++ rubiks_cube/main.cpp
@@ -148,7 +148,7 @@ float lastFrame = 0.0f;
 
 // lighting
 glm::vec3 lightPos(2.4f, 2.4f, 2.4f);
-bool lightMov = true;
+bool lightMov = false;
 
 HWND hwndGL;
 WNDPROC OldProc;
@@ -228,7 +228,7 @@ int main()
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Use for debug
 
     // Menu
     hwndGL = GetActiveWindow();
@@ -464,6 +464,15 @@ int main()
        -0.5f,  0.5f,  0.5f,
        -0.5f,  0.5f, -0.5f,
     };
+
+    // positions of the point lights
+    glm::vec3 pointLightPositions[] = {
+        glm::vec3(2.3f, -3.3f, -4.0f),
+        glm::vec3(2.5f,  0.0f,  0.0f),
+        glm::vec3(0.0f,  0.0f,  2.5f),
+        glm::vec3(0.0f,  2.5f,  0.0f)
+    };
+
     // skybox VAO
     unsigned int skyboxVAO, skyboxVBO;
     glGenVertexArrays(1, &skyboxVAO);
@@ -541,9 +550,9 @@ int main()
 
         if (lightMov == true) 
         {
-            lightPos.x = sin(glfwGetTime()) * cos(glfwGetTime() / 2.0f) * 2.7f;
-            lightPos.y = cos(glfwGetTime()) * sin(glfwGetTime() / 2.0f) * 2.7f;
-            lightPos.z = cos(glfwGetTime()) * 2.7f;
+            pointLightPositions[0].x = sin(glfwGetTime()) * cos(glfwGetTime() / 2.0f) * 5.2f;
+            pointLightPositions[0].y = cos(glfwGetTime()) * sin(glfwGetTime() / 2.0f) * 5.2f;
+            pointLightPositions[0].z = cos(glfwGetTime()) * 5.2f;
         }
 
         // view/projection transformations
@@ -553,9 +562,69 @@ int main()
 
         // activate shader
         cubeShader.use();
-        cubeShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-        cubeShader.setVec3("lightPos", lightPos);
         cubeShader.setVec3("viewPos", camera.Position);
+        
+        /*
+           Here we set all the uniforms for the 5/6 types of lights we have. We have to set them manually and index
+           the proper PointLight struct in the array to set each uniform variable. This can be done more code-friendly
+           by defining light types as classes and set their values in there, or by using a more efficient uniform approach
+           by using 'Uniform buffer objects', but that is something we'll discuss in the 'Advanced GLSL' tutorial.
+        */
+        // directional light
+        cubeShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+        cubeShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+        cubeShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+        cubeShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+        // point light 1
+        cubeShader.setVec3("pointLights[0].position", pointLightPositions[0]);
+        cubeShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+        cubeShader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+        cubeShader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+        cubeShader.setFloat("pointLights[0].constant", 1.0f);
+        cubeShader.setFloat("pointLights[0].linear", 0.09);
+        cubeShader.setFloat("pointLights[0].quadratic", 0.032);
+        // point light 2
+        cubeShader.setVec3("pointLights[1].position", pointLightPositions[1]);
+        cubeShader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
+        cubeShader.setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
+        cubeShader.setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+        cubeShader.setFloat("pointLights[1].constant", 1.0f);
+        cubeShader.setFloat("pointLights[1].linear", 0.09);
+        cubeShader.setFloat("pointLights[1].quadratic", 0.032);
+        // point light 3
+        cubeShader.setVec3("pointLights[2].position", pointLightPositions[2]);
+        cubeShader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
+        cubeShader.setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
+        cubeShader.setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
+        cubeShader.setFloat("pointLights[2].constant", 1.0f);
+        cubeShader.setFloat("pointLights[2].linear", 0.09);
+        cubeShader.setFloat("pointLights[2].quadratic", 0.032);
+        // point light 4
+        cubeShader.setVec3("pointLights[3].position", pointLightPositions[3]);
+        cubeShader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
+        cubeShader.setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
+        cubeShader.setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+        cubeShader.setFloat("pointLights[3].constant", 1.0f);
+        cubeShader.setFloat("pointLights[3].linear", 0.09);
+        cubeShader.setFloat("pointLights[3].quadratic", 0.032);
+        // spotLight
+        cubeShader.setVec3("spotLight.position", camera.Position);
+        cubeShader.setVec3("spotLight.direction", camera.Front);
+        cubeShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+        cubeShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+        cubeShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+        cubeShader.setFloat("spotLight.constant", 1.0f);
+        cubeShader.setFloat("spotLight.linear", 0.09);
+        cubeShader.setFloat("spotLight.quadratic", 0.032);
+        cubeShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+        cubeShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
+
+        // material properties
+        cubeShader.setVec3("material.ambient", 0.25f, 0.20725f, 0.20725f);
+        cubeShader.setVec3("material.diffuse", 1.0f, 0.829f, 0.829f);
+        cubeShader.setVec3("material.specular", 0.296648f, 0.296648f, 0.296648f); // specular lighting doesn't have full effect on this object's material
+        cubeShader.setFloat("material.shininess", 32.0f);
+
         // camera/view transformation
         view = glm::lookAt(camera.Position, glm::vec3(0.f, 0.f, 0.f), camera.Up);
         cubeShader.setMat4("view", view);
@@ -898,12 +967,15 @@ int main()
         lightCubeShader.use();
         lightCubeShader.setMat4("projection", projection);
         lightCubeShader.setMat4("view", view);
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos);
-        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-        lightCubeShader.setMat4("model", model);
         glBindVertexArray(lightCubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 4; i++)
+        {
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, pointLightPositions[i]);
+            model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+            lightCubeShader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         // draw skybox as last
         glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
@@ -929,6 +1001,7 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &cubeVAO);
     glDeleteBuffers(1, &cubeVBO);
+    glDeleteBuffers(1, &colorbuffer);
     glDeleteVertexArrays(1, &skyboxVAO);
     glDeleteBuffers(1, &skyboxVBO);
     glDeleteVertexArrays(1, &lightCubeVAO);
